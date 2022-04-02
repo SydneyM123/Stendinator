7b8283f968f97d499838ecf2ec66718de62ad7a4
HBembom <56027034+HBembom@users.noreply.github.com>
2022-04-02 13:47:58 +0200

    Craeted Abstract Factories

---
+++ Stendinator/Stendinator.Core/Components/Factories/IAbstractComponentFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Stendinator.Core.Components.Factories
+{
+    public interface IAbstractComponentFactory
+    {
+        Component CreateLeg();
+        Component CreateHead();
+        Component CreateBody();
+    }
+}
--- Stendinator/Stendinator.Core/Creatures/Factories/RandomAlienFactory.cs
+++ Stendinator/Stendinator.Core/Creatures/Factories/RandomAlienFactory.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Stendinator.Core.Components;
 using Stendinator.Core.Components.Factories;
+using Stendinator.Core.Components.Factories.AbstractComponentFactories;
+using Stendinator.Core.Components.Factories.ConcreteFactories;
 using Stendinator.Core.Components.Heads;
 using Stendinator.Core.Creatures.Aliens;
 
@@ -10,20 +12,73 @@ namespace Stendinator.Core.Creatures.Factories
 {
     public class RandomAlienFactory : IRandomCreatureFactory
     {
-        private readonly ComponentFactory _componentFactory = new ComponentFactory();
+        // Perhaps Make Abstract Factory for Balanced, Healthy, and Sturdy
+        private readonly ComponentFactory[] _componentFactories;
+        private readonly IAbstractComponentFactory[] _abstractComponentFactories;
+        
+        public RandomAlienFactory()
+        {
+            _componentFactories = new ComponentFactory[]
+            {
+                new ConcreteChainSawArm(),
+                new ConcreteFlameThrowerArm(),
+                new ConcreteHealingArm(),
+                new ConcreteShieldArm(),
+                new ConcreteStunGunArm(),
+            };
+
+            _abstractComponentFactories = new IAbstractComponentFactory[]
+            {
+                new SturdyComponentFactory(),
+                new HealthyComponentFactory(),
+                new BalancedComponentFactory()
+            };
+        }
+
         public Creature Create()
         {
-            var alien = new Alien();
-            var random = new Random();
 
+            var alien = new Alien();
+            alien.AddComponent(GetRandomBody());
+            alien.AddComponent(GetRandomLeg());
+            alien.AddComponent(GetRandomHead());
+            
             //Make 5 random components
             for (var i = 0; i < 5; i++)
             {
-                var getRandomName = _componentFactory.GetNames();
-                alien.AddComponent( _componentFactory.Create(getRandomName[random.Next(getRandomName.Length)]));
+                
             }
 
             return alien;
+        }
+
+        private Component GetRandomArm()
+        {
+            Random random = new Random();
+            int index = random.Next(0, _componentFactories.Length);
+            return _componentFactories[index].Create();
+        }
+
+
+        private Component GetRandomLeg()
+        {
+            Random random = new Random();
+            int index = random.Next(0, _componentFactories.Length);
+            return _abstractComponentFactories[index].CreateLeg();
+        }
+
+        private Component GetRandomHead()
+        {
+            Random random = new Random();
+            int index = random.Next(0, _componentFactories.Length);
+            return _abstractComponentFactories[index].CreateHead();
+        }
+
+        private Component GetRandomBody()
+        {
+            Random random = new Random();
+            int index = random.Next(0, _componentFactories.Length);
+            return _abstractComponentFactories[index].CreateBody();
         }
     }
 }
