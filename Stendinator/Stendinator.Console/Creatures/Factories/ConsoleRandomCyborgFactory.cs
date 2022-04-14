102e900b6c0a5ef7a07f5184bdf057c0e7508ea9
Sydney <sydneyminnaar2000@hotmail.com>
2022-04-14 16:47:15 +0200

    Renamed RandomCyborgFactory - Copy to ConsoleRandomCyborgFactory

---
+++ Stendinator/Stendinator.Console/Creatures/Factories/ConsoleRandomCyborgFactory.cs
@@ -0,0 +1,19 @@
+﻿using Stendinator.Console.Components;
+using Stendinator.Core.Components.Factories;
+using Stendinator.Core.Creatures;
+using Stendinator.Core.Creatures.Factories;
+
+namespace Stendinator.Console.Creatures.Factories
+{
+    internal class ConsoleRandomCyborgFactory : RandomCyborgFactory
+    {
+        public ConsoleRandomCyborgFactory(IRandomComponentFactory randomComponentFactory) : base(randomComponentFactory)
+        {
+        }
+
+        public override Creature Create()
+        {
+            return new ConsoleCreature(base.Create());
+        }
+    }
+}
